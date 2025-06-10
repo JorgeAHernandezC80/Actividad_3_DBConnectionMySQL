77ce8e715119394813f6b11da2b915fcb49e6648
JorgeAHernandezC80 <jotache80@icloud.com>
2025-06-10 12:29:34 -0500

    Add MySQL DB connection utility class

--- src/main/java/edu/iudigital/DBConnection.java
+++ src/main/java/edu/iudigital/DBConnection.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBConnection {
+public class DBConnection{
     // Configuración de conexión
     private static final String URL = "jdbc:mysql://localhost:3306/registro_profesores"; // Cambia por tu BD
     private static final String USUARIO = "root";
